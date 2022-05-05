decbced227e3aa8daf1d3b018d4518745d9e77c5
Don_Silva <83092440+Silvadon101@users.noreply.github.com>
2022-05-05 12:58:41 -0500

    Add Google Signup Button

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +13,37 @@
     <link href="/css/formpop.css" rel="stylesheet">
     <script src="/js/jquery.waypoints.min.js" type="text/javascript"></script>
     <script src="/js/waypoints.js" type="text/javascript"></script>
+
+    <style>
+        /* ------Google Btn------ */
+        .google-icon-wrapper {
+            position: absolute;
+            margin-top: 1px;
+            margin-left: 1.5px;
+            width: 40px;
+            height: 40px;
+            border-radius: 2px;
+            border-right: 1px solid whitesmoke;
+        }
+
+        .google-icon {
+            position: absolute;
+            margin-top: 11px;
+            margin-left: 11px;
+            width: 18px;
+            height: 18px;
+        } 
+
+        .google-btn {
+            width: 62%;
+            height: 42px;
+            border-radius: 2px;
+            /* box-shadow: 0 3px 4px 0 rgba(0, 0, 0, .25); */
+        }
+
+    </style>
 </head>
+
 <body>
     {{-- --------------------------Focus Section------------------------ --}}
     <section class="intro">
@@ -35,36 +66,44 @@
     {{-- ---------------------Login Form----------------------- --}}
     <section>
         <div class="popup">
-          <div class="close-btn">&times;</div>
-              <div class="form">
-                    <form action="login" method="POST">
-                        @csrf
-                        <h2>Log in</h2>
-                        <div class="form-element username">
-                          <label for="usern">Username</label><br>
-                            <span style="color:rgb(235, 52, 52)">
-                                @if (Session::get('faillog'))
-                                    {{ Session::get('faillog') }}
-                                @endif
-                            </span>
-                            <div style="color:rgb(235, 52, 52)">@error('usernam') {{$message}} @enderror </div>
-                          <input type="text" id="usern"  name="usernam" placeholder="dev_newbie_john" >
-                          <div class="error error-txt">Wait! you need a username</div>
+            <div class="close-btn">&times;</div>
+            <div class="form">
+                <form action="login" method="POST">
+                    @csrf
+                    <h2>Log in</h2>
+                    <div class="form-element username">
+                        <label for="usern">Username</label><br>
+                        <span style="color:rgb(235, 52, 52)">
+                            @if (Session::get('faillog'))
+                                {{ Session::get('faillog') }}
+                            @endif
+                        </span>
+                        <div style="color:rgb(235, 52, 52)">
+                            @error('usernam')
+                                {{ $message }}
+                            @enderror
                         </div>
-                        <div class="form-element password">
-                          <label for="password">Password</label>
-                          <div style="color:rgb(235, 52, 52)">@error('pw') {{$message}} @enderror </div>
-                          <input type="password" id="password"  name="pw" placeholder="password">
-                            <div class="error error-txt">Wait! you need a password</div>
+                        <input type="text" id="usern" name="usernam" placeholder="dev_newbie_john">
+                        <div class="error error-txt">Wait! you need a username</div>
+                    </div>
+                    <div class="form-element password">
+                        <label for="password">Password</label>
+                        <div style="color:rgb(235, 52, 52)">
+                            @error('pw')
+                                {{ $message }}
+                            @enderror
                         </div>
-                        <div class="form-element">
-                          <button type="submit">SIGN IN</button>
-                        </div>
-                        <div class="form-element">
-                          <button type="reset">RESET</button>
-                        </div>
-                    </form>
-              </div>
+                        <input type="password" id="password" name="pw" placeholder="password">
+                        <div class="error error-txt">Wait! you need a password</div>
+                    </div>
+                    <div class="form-element">
+                        <button type="submit">SIGN IN</button>
+                    </div>
+                    <div class="form-element">
+                        <button type="reset">RESET</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </section>
     {{-- -----------X----------Login Form-----------X------------ --}}
@@ -72,57 +111,80 @@
     {{-- ----------------------Sign Up Form---------------------- --}}
     <section>
         <div class="popup-two">
-          <div class="close-btn-two">&times;</div>
-              <div class="form">
-                    <form action="signup" class="form_bd" method="POST">
-                        @csrf
-                        <h2>Sign up</h2>
-                        <div class="form-element fullname">
+            <div class="close-btn-two">&times;</div>
+            <div class="form">
+                <form action="signup" class="form_bd" method="POST">
+                    @csrf
+                    <h2>Sign up</h2>
+                    <div class="form-element fullname">
                         <label for="nam">Full Name</label>
-                        <div style="color:rgb(235, 52, 52)">@error('name') {{$message}} @enderror </div>
+                        <div style="color:rgb(235, 52, 52)">
+                            @error('name')
+                                {{ $message }}
+                            @enderror
+                        </div>
                         <input type="text" id="nam" name="name" placeholder="John Doe">
                         <div class="error error-txt">Wait! your name can't be blank</div>
+                    </div>
+                    <div class="form-element username_two">
+                        <label for="user">Username</label><br>
+                        <span style="color:rgb(235, 52, 52)">
+                            @if (Session::get('failuser'))
+                                {{ Session::get('failuser') }}
+                            @endif
+                        </span>
+                        <div style="color:rgb(235, 52, 52)">
+                            @error('username')
+                                {{ $message }}
+                            @enderror
                         </div>
-                        <div class="form-element username_two">
-                          <label for="user">Username</label><br>
-                            <span style="color:rgb(235, 52, 52)">
-                                @if (Session::get('failuser'))
-                                    {{ Session::get('failuser') }}
-                                @endif
-                            </span>
-                            <div style="color:rgb(235, 52, 52)">@error('username') {{$message}} @enderror </div>
-                            <input type="text" id="user" name="username" placeholder="dev_newbie_john">
-                          <div class="error error-txt">Wait! your username can't be blank</div>
+                        <input type="text" id="user" name="username" placeholder="dev_newbie_john">
+                        <div class="error error-txt">Wait! your username can't be blank</div>
+                    </div>
+                    <div class="form-element email">
+                        <label for="mail">Email</label><br>
+                        <span style="color:rgb(235, 52, 52)">
+                            @if (Session::get('failemail'))
+                                {{ Session::get('failemail') }}
+                            @endif
+                        </span>
+                        <div style="color:rgb(235, 52, 52)">
+                            @error('email')
+                                {{ $message }}
+                            @enderror
                         </div>
-                        <div class="form-element email">
-                          <label for="mail">Email</label><br>
-                            <span style="color:rgb(235, 52, 52)">
-                                @if (Session::get('failemail'))
-                                    {{ Session::get('failemail') }}
-                                @endif
-                            </span>
-                            <div style="color:rgb(235, 52, 52)">@error('email') {{$message}} @enderror </div>
-                          <input type="email" id="mail" name="email" placeholder="johndoe@gmail.com">
-                          <div class="error error-txt">Wait! your email can't be blank</div>
+                        <input type="email" id="mail" name="email" placeholder="johndoe@gmail.com">
+                        <div class="error error-txt">Wait! your email can't be blank</div>
+                    </div>
+                    <div class="form-element password_two">
+                        <label for="password">Password</label>
+                        <div style="color:rgb(235, 52, 52)">
+                            @error('password')
+                                {{ $message }}
+                            @enderror
                         </div>
-                        <div class="form-element password_two">
-                          <label for="password">Password</label>
-                          <div style="color:rgb(235, 52, 52)">@error('password') {{$message}} @enderror </div>
-                          <input type="password" id="password" name="password" placeholder="password">
-                          <div class="error error-txt">Wait! your password can't be blank</div>
+                        <input type="password" id="password" name="password" placeholder="password">
+                        <div class="error error-txt">Wait! your password can't be blank</div>
+                    </div>
+                    <div class="form-element">
+                        <label for="">Upload Profile Image</label>
+                        <input type="file" name="img">
+                    </div>
+                    <div class="form-element">
+                        <button type="submit">REGISTER</button>
+                    </div>
+                    <div class="form-element">
+                        <button type="reset">RESET</button>
+                    </div>
+                    <div class="form-element google-btn">
+                        <div class="google-icon-wrapper">
+                            <img src="/img/google-icon.png" class="google-icon" />
                         </div>
-                        <div class="form-element">
-                          <label for="">Upload Profile Image</label>
-                          <input type="file" name="img">
-                        </div>
-                        <div class="form-element">
-                          <button type="submit">REGISTER</button>
-                        </div>
-                        <div class="form-element">
-                          <button type="reset">RESET</button>
-                        </div>
-                    </form>
-              </div>
+                        <button type="submit">Sign in with google</button>
+                    </div>
+
+                </form>
+            </div>
         </div>
     </section>
     {{-- ---------X------------Sign Up Form----------X----------- --}}
@@ -132,11 +194,12 @@
     <script src="/js/formpop2.js" type="text/javascript"></script>
 
     {{-- ----------Forms Validation -------- --}}
-     {{-- <script src="/js/validate.js"></script> --}}
-     {{-- <script src="/js/validatetwo.js"></script> --}}
+    {{-- <script src="/js/validate.js"></script> --}}
+    {{-- <script src="/js/validatetwo.js"></script> --}}
 
     {{-- --------Footer-------- --}}
     @include('footer')
 
 </body>
+
 </html>
